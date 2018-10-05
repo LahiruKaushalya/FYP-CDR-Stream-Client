6c1acb1ae3aba8c0632769f0a4ac85bfdb1c5c40
shankajarajapakse <sankaja.14@cse.mrt.ac.lk>
2018-10-05 12:48:42 -0700

    table checking mechanism updated

--- src/main/java/com/fypcdr/app/stream/client/PostgreConnector.java
+++ src/main/java/com/fypcdr/app/stream/client/PostgreConnector.java
@@ -27,27 +27,15 @@ public class PostgreConnector{
     public static void createTable(String tablename) throws SQLException {
         globaltablename = tablename;
         Statement statement = getConnection().createStatement();
-        if (tableAlreadyExists())
-        {
-            System.out.println("Table "+tablename+" alredy exists \nFlushing data to receive new records\n");
-            String query = "DELETE * FROM "+tablename+";";
-            statement.executeUpdate(query);
-
-            System.out.println("Flushing completed ..");
-        }
-        else
-        {
-
-            String sql = "CREATE TABLE " + tablename +
-                    "(called_num        CHAR(64)," +
-                    " called_tower      CHAR(6), " +
-                    " recipient_num     CHAR(64), " +
-                    " recipient_tower   CHAR(6), " +
-                    " datetime          CHAR(29)," +
-                    " duration          CHAR(10))";
-            statement.executeUpdate(sql);
-            System.out.println("Table created as " + tablename);
-        }
+        String sql = "CREATE TABLE IF NOT EXISTS " + tablename +
+                "(called_num        CHAR(64)," +
+                " called_tower      CHAR(6), " +
+                " recipient_num     CHAR(64), " +
+                " recipient_tower   CHAR(6), " +
+                " datetime          CHAR(29)," +
+                " duration          CHAR(10))";
+        statement.executeUpdate(sql);
+        System.out.println("Table created as " + tablename);
         statement.close();
     }
 
@@ -55,18 +43,6 @@ public class PostgreConnector{
         return globaltablename;
     }
 
-    public static boolean tableAlreadyExists() throws SQLException {
-        DatabaseMetaData dbm = getConnection().getMetaData();
-        // check if table is there
-        ResultSet tables = dbm.getTables(null, null, globaltablename, null);
-        if (tables.next()) {
-            // Table exists
-            return true;
-        }
-        else {
-            // Table does not exist
-            return false;
-        }
-    }
+
 
 }
