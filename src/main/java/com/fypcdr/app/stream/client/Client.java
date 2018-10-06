0f23351c936c74ba05eccf9173a09a8d6f30ed24
PushpithaDilhan <pushpitha.14@cse.mrt.ac.lk>
2018-10-07 00:39:40 +0530

    time measuring added

--- src/main/java/com/fypcdr/app/stream/client/Client.java
+++ src/main/java/com/fypcdr/app/stream/client/Client.java
@@ -10,14 +10,16 @@ import java.util.UUID;
 public class Client {
 
     public static void main(String[] args) throws SQLException {
+        Long x = System.currentTimeMillis();
 
         int noOfRecords = Integer.parseInt(args[0]);
         String tablename = "cdrtesttable";
         PostgreConnector.createTable(tablename);
-
         StreamHandler streamHandler = new StreamHandler();
-
         streamHandler.requestCDRRecords(noOfRecords);
+
+        Long y = System.currentTimeMillis();
+        System.out.println((y-x)/1000 + " Seconds taken.");
     }
 
 
